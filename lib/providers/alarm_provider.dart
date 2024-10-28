5525a3cc7afc855775c295e8987124fcc7e5ffbd
Darshan Varu <darshanvaru2003@gmail.com>
2024-10-28 16:37:21 +0530

    Final Editing before delivering

--- lib/providers/alarm_provider.dart
+++ lib/providers/alarm_provider.dart
@@ -1,4 +1,4 @@
-import 'package:alarmclock/screens/notification_screen.dart';
+// import 'package:alarmclock/screens/notification_screen.dart';
 import 'package:flutter/material.dart';
 import '../models/alarm_model.dart';
 import '../services/notification_service.dart';
